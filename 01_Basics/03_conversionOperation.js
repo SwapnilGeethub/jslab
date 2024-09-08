45b4bae5ca62ea44121ec6447cf83217cb052306
Swapnil Gaidhani <49488730+SwapnilGeethub@users.noreply.github.com>
2024-09-08 18:55:08 +0000

    operations

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -23,7 +23,53 @@ let score = true;
  * "swapnil" => true * 
  */
 
-let someNumber = 33
-let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//let someNumber = 33
+//let stringNumber = String(someNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// ******************* Operations *******************
+
+let value = 3;
+let negValue = -value;
+
+//console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello";
+let str2 = " swapnil";
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true); //=> 1
+// console.log(+""); //=> 0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter); //learn about increment from mdn web docs
+
+/** Link to Study
+ * https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+ */
+
+
+
+
+
+
+
+
