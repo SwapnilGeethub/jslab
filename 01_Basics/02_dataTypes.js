82590603ee86a1d209c7ca4c3185540e0e05346a
Swapnil Gaidhani <49488730+SwapnilGeethub@users.noreply.github.com>
2024-09-08 18:19:28 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+/**
+ * alert("hello"); //It won't work with node.js
+ * reason: browser don't have window object, and alert is a function associated with window object that is only present in browser.
+ */
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Swapnil");
+let name = "omkar"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
