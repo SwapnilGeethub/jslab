91e657680313505b30090247e1fd2bda2aa7e9da
Swapnil Gaidhani <49488730+SwapnilGeethub@users.noreply.github.com>
2024-09-09 14:35:57 +0000

    Stack & Heap Explained

--- 01_Basics/datatypes-summary.js
+++ 01_Basics/datatypes-summary.js
@@ -50,4 +50,34 @@
        Arrays  =>  object
        Function  =>  function (object function)
        Object  =>  object 
- * */ 
+ * */
+
+/**
+ * Stack (Primitive)
+ * => Usually primitives kept in stack.
+ * => Values only be copied during assignment
+ * 
+ * Heap (Non-Primitive)
+ * => Usually non-primitives kept in Heap.
+ * => Values only be refered during assignment, but not copied
+ * 
+*/
+
+let myName = "Swapnil"; // => variable kept in Stack
+let anotherName = myName; // => value is copied
+
+anotherName = "Omkar"; // => value replaced
+console.log(myName);    // => Swapnil
+console.log(anotherName); //=> Omkar
+
+
+let userOne = {   //=> Object kept in Heap
+      email: "swapnil@one.com",
+      upi:"userone@tbl"
+}
+
+let userTwo = userOne;  //=> Object memory reference is given
+userTwo.email = "omkar@one.com"
+
+console.log(userOne.email); //omkar@one.com
+console.log(userTwo.email); //omkar@one.com
