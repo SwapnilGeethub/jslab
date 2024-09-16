abb272cf4e7b4e6d54d1eb92a2c2d767d2a36402
Swapnil Gaidhani <49488730+SwapnilGeethub@users.noreply.github.com>
2024-09-16 19:26:18 +0000

    Arrow Function comprehensive

--- 03_Basics/03_arrowfunction.js
+++ 03_Basics/03_arrowfunction.js
@@ -2,45 +2,60 @@ const user = {
     username: "hemant",
     price: 999,
 
-    welcomeMessage: function(){
+    // Method to log a welcome message for the user
+    welcomeMessage: function() {
+        // 'this' refers to the object (user) that calls the method
         console.log(`${this.username}, Welcome to website`);        
-        console.log(this);
-        
-    } 
+        console.log(this); // logs the current object context (user object)
+    }
 }
 
-// user.welcomeMessage();
-// user.username = "Sameer";
-// user.welcomeMessage();
+// Uncomment the following lines to see behavior:
 
-// console.log(this);
+// user.welcomeMessage(); // Calls the method, logs the original 'username' (hemant) and object
 
-// function coffee(){
-//     let username = "hemant";
-//     console.log(this.username);
-// }
+// user.username = "Sameer"; // Updates the 'username' property of the user object
 
-// coffee();
+// user.welcomeMessage(); // Now logs the updated 'username' (Sameer) and the object
 
-// const juice = function (){
-//     let username = "hemant";
-//     console.log(this.username);
-// }
-// juice()
-// const milk = () => {
-//     let username = "hemant";
-//     console.log(this);
-// }
-// milk()
+// console.log(this); // In the global scope, 'this' refers to the global object in browsers (or undefined in strict mode)
 
+// Function declaration (non-arrow function)
+function coffee(){
+    let username = "hemant"; // Local variable
+    // In a non-method function, 'this' refers to the global object (window in browser)
+    console.log(this.username); // undefined, since 'this' doesn't refer to the current function's scope
+}
 
+coffee(); // 'this' is the global object, so it tries to access 'username' from the global scope, resulting in undefined
+
+// Function expression (non-arrow function)
+const juice = function (){
+    let username = "hemant"; // Local variable
+    console.log(this.username); // undefined, since 'this' still refers to the global object
+}
+juice(); // Behaves similarly to 'coffee()' due to non-arrow function
+
+// Arrow function
+const milk = () => {
+    let username = "hemant"; // Local variable
+    console.log(this); // In arrow functions, 'this' is lexically bound (inherits from surrounding context, not global object)
+}
+milk(); // Logs the outer context (likely the global object or undefined depending on the environment)
+
+// Function with an explicit return
+// Different styles of writing the same function using arrow function syntax
 // const addTwo = (num1, num2) => {
 //     return num1 + num2;
 // }
-// const addTwo = (num1, num2) => num1 + num2; // implicit return
+
+// Shortened syntax: implicit return (no need for curly braces or 'return' keyword)
+//const addTwo = (num1, num2) => num1 + num2; // implicit return
+
+// Can also use parentheses for implicit return if returning an expression
 //const addTwo = (num1, num2) => (num1 + num2); // implicit return
-const addTwo = (num1, num2) => ({username: "hemant"}); // implicit return
 
-console.log(addTwo(3,4));
+// Returning an object: when returning an object in arrow functions, wrap it in parentheses
+const addTwo = (num1, num2) => ({ username: "hemant" }); // implicit return of an object
 
-
+console.log(addTwo(3,4)); // Logs the object {username: "hemant"}
